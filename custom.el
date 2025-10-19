99874e17d28d769fdace590b1471480e63cab46f
defclass <defclass@outlook.com>
2025-10-20 01:12:15 +0900

    fix: add some new topics

--- custom.el
+++ custom.el
@@ -15,37 +15,52 @@
 
 (org-topic-register
  :prefix "tools"
- :root-dir (expand-file-name "technology/tools" org-note-dir)
+ :root-dir (expand-file-name "technology/tools" org-roam-topic-dir)
+ :org-roam-type t
  :template "default-org"
  :prompt "输入工具主题: ")
 
 (org-topic-register
  :prefix "hacklog"
- :root-dir (expand-file-name "technology/hacklog" org-note-dir)
+ :root-dir (expand-file-name "technology/hacklog" org-roam-topic-dir)
  :template "default-org"
+ :org-roam-type t
  :prompt "输入探索主题: ")
 
 (org-topic-register
  :prefix "journal"
- :root-dir (expand-file-name "daily" org-note-dir)
+ :root-dir (expand-file-name "journal" org-roam-topic-dir)
  :date t
  :title-enabled nil
+ :org-roam-type t
  :title-prefix "Journal"
  :template "default-life-daily"
  :prompt "选择或输入日期: ")
-
-(org-topic-register
- :prefix "project"
- :root-dir (expand-file-name "project" org-note-dir)
- :date t
- :title-enabled t
- :title-prefix "project"
- :template "default-org"
- :prompt "选择或输入项目名: ")
 
 (defun daily (&optional arg)
   (interactive "P")
   (let ((today (format-time-string "%Y-%m-%d")))
     (journal/open-theme today)))
+
+(org-topic-register
+ :prefix "project"
+ :root-dir (expand-file-name "project" org-roam-topic-dir)
+ :date t
+ :org-roam-type t
+ :title-enabled t
+ :title-prefix "project"
+ :template "default-org"
+ :prompt "选择或输入项目名: ")
+
+
+(org-topic-register
+ :prefix "reading"
+ :root-dir (expand-file-name "Reading" org-roam-topic-dir)
+ :org-roam-type t
+ :title-enabled t
+ :title-prefix "Reading"
+ :template "default-org"
+ :prompt "输入书名: ")
+
 
 (require 'init-blog)
